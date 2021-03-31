7bb06c1a389e5006cba0f9b7cf7f1d32f4537720
Quoc Huy <saviorlight271@gmail.com>
2021-03-31 23:43:21 +0700

    component header menu

---
+++ src/ui/organisms/header/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {HomeFilled} from '@ant-design/icons'
+import {MessageFilled} from '@ant-design/icons'
+import {NotificationFilled} from '@ant-design/icons'
+import {UserOutlined} from '@ant-design/icons'
+import {SendOutlined} from '@ant-design/icons'
+import {CaretDownOutlined} from '@ant-design/icons'
+import { Button } from 'antd';
+import './header.scss'
+export default function Header() {
+    return (
+        <div id="menu">
+            <ul>
+                <li><a className="logo" href="#"><HomeFilled /></a></li>
+                <li><a href="#"><MessageFilled /></a></li>
+                <li><a href="#"><NotificationFilled /></a></li>
+                <Button className="btn-guithu" type="primary">Gửi thư nhanh <SendOutlined /></Button>  
+                <li>
+                    <a href="#">Cài đặt <CaretDownOutlined /></a>
+                    <ul className="sub-menu">
+                        <li><a href="#">Hệ thống</a></li>
+                        <li><a href="#">Backup</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Q.lý Module <CaretDownOutlined /></a>
+                    <ul className="sub-menu">
+                        <li><a href="#">Module</a></li>
+                        <li><a href="#">Thêm Module</a></li>
+                    </ul>
+                </li>
+                <li className="admin">
+                    <a href="#">Admin <UserOutlined /> <CaretDownOutlined /></a>
+                    <ul className="sub-menu">
+                        <li><a href="#">Edit info</a></li>
+                        <li><a href="#">Log out</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    )
+}
