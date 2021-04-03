a6401e57e5535274db687e5dd3a66d68bfe9f0a9
duytranndev <duytrann.dev@gmail.com>
2021-04-03 11:37:11 +0700

    commit

--- src/share/routing/index.tsx
+++ src/share/routing/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import HomePage from '../../pages/home'
+import ManagementDepartment from '../../ui/organisms/department/management'
 
 export default function AppRouting() {
   const routeList = [
     {
       component: <Route path='/' component={HomePage} exact />
+    },
+    {
+      component: <Route path='/department' component={ManagementDepartment} exact />
     }
   ]
   return (
