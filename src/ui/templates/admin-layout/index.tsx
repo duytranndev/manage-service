02735069d0fdab027e1469fff62f497307bd9d33
duytranndev <duytrann.dev@gmail.com>
2021-04-05 15:11:15 +0700

    commit

--- src/ui/templates/admin-layout/index.tsx
+++ src/ui/templates/admin-layout/index.tsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons'
+import { Layout } from 'antd'
+import 'antd/dist/antd.css'
+import { Header } from 'antd/lib/layout/layout'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import AdminRouting from '../../../share/routing/admin'
+import HeaderAdmin from '../../../ui/organisms/header'
+import MenuAdmin from '../../../ui/organisms/menu'
+// import './index.scss'
+
+const { Sider, Content } = Layout
 
 export default function AdminLayout() {
+  const [collapsed, setCollapsed] = useState(false)
+  const toggle = () => {
+    setCollapsed(!collapsed)
+  }
   return (
     <>
-      <div className='page-content'>
-        <AdminRouting />
-      </div>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <Link to='/admin'>
+            <div className='logo'>This is Logo</div>
+          </Link>
+
+          <MenuAdmin />
+        </Sider>
+
+        <Layout className='site-layout'>
+          <HeaderAdmin />
+          <Header className='site-layout-background' style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggle
+            })}
+          </Header>
+          <Content
+            className='site-layout-background'
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280
+            }}>
+            <AdminRouting />
+          </Content>
+        </Layout>
+      </Layout>
     </>
   )
 }
